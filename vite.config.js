7df40e08df5a04fcc25ceb0b0093ab5bab9fa1a1
Greg-goprod <contact@gc-entertainment.ch>
2025-05-20 13:15:16 +0200

    Fix Tailwind Build Error

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-import tailwindcss from '@tailwindcss/vite'
-
 // https://vite.dev/config/
 export default defineConfig({
   base: "/admin-one-vue-tailwind/",
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  }
 })
